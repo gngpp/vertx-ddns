4079251cb53f34e50119eed976d49b8751f3e516
zf1976 <verticle@foxmail.com>
2021-07-25 14:52:38 +0800

    refactor: refactor some api

--- src/main/java/com/zf1976/ddns/Application.java
+++ src/main/java/com/zf1976/ddns/Application.java
@@ -10,8 +10,7 @@ import io.vertx.core.Vertx;
 public class Application {
 
     public static void main(String[] args) {
-        Vertx.vertx()
-             .deployVerticle(new ConfigVerticle(args));
+        Vertx.vertx().deployVerticle(new ConfigVerticle(args));
     }
 
 }
--- src/main/java/com/zf1976/ddns/api/HuaweiDnsAPI.java
+++ src/main/java/com/zf1976/ddns/api/HuaweiDnsAPI.java
@@ -7,7 +7,9 @@ import com.zf1976.ddns.api.enums.MethodType;
 import com.zf1976.ddns.api.signer.HuaweiRequest;
 import com.zf1976.ddns.pojo.HuaweiDataResult;
 import com.zf1976.ddns.util.CollectionUtil;
+import com.zf1976.ddns.util.StringUtil;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.logging.log4j.LogManager;
@@ -28,9 +30,8 @@ public class HuaweiDnsAPI extends AbstractDnsAPI {
 
     private final Logger log = LogManager.getLogger("[HuaweiDnsAPI]");
     private final String api = "https://dns.myhuaweicloud.com/v2/zones";
-    private final CloseableHttpClient closeableHttpClient = HttpClients.custom()
-                                                                       .build();
-    private final Map<String, String> zoneMap;
+    private final CloseableHttpClient closeableHttpClient = HttpClients.custom().build();
+    private final Map<String, String> zoneMap = new HashMap<>();
 
     public HuaweiDnsAPI(String id, String secret) {
         this(new BasicCredentials(id, secret));
@@ -38,52 +39,45 @@ public class HuaweiDnsAPI extends AbstractDnsAPI {
 
     public HuaweiDnsAPI(DnsApiCredentials dnsApiCredentials) {
         super(dnsApiCredentials);
-        this.zoneMap = new HashMap<>();
-
-        CloseableHttpResponse httpResponse = null;
         try {
             final var httpRequestBase = HuaweiRequest.newBuilder(dnsApiCredentials)
-                                                     .setUrl(api)
-                                                     .setMethod(MethodType.GET)
-                                                     .build();
-            httpResponse = this.closeableHttpClient.execute(httpRequestBase);
-            if (httpResponse.getStatusLine()
-                            .getStatusCode() == 200) {
-                if (httpResponse.getEntity() != null) {
-                    final var contentBytes = this.getContentBytes(httpResponse);
-                    final var huaweiDataResult = this.mapperResult(contentBytes, HuaweiDataResult.class);
-                    final var zones = huaweiDataResult.getZones();
-                    if (CollectionUtil.isEmpty(zones)) {
-                        throw new RuntimeException("Failed to get zone id");
-                    } else {
-                        // 按域名划分区域
-                        for (HuaweiDataResult.Zones zone : zones) {
-                            var domain = zone.getName();
-                            domain = domain.substring(0, domain.length() - 1);
-                            this.zoneMap.put(domain, zone.getId());
-                        }
-                    }
+                    .setUrl(api)
+                    .setMethod(MethodType.GET)
+                    .build();
+            final var contentBytes = this.executeRequest(httpRequestBase);
+            final var huaweiDataResult = this.mapperResult(contentBytes, HuaweiDataResult.class);
+            final var zones = huaweiDataResult.getZones();
+            if (CollectionUtil.isEmpty(zones)) {
+                throw new RuntimeException("Failed to get zone id");
+            } else {
+                // 按域名划分区域
+                for (HuaweiDataResult.Zones zone : zones) {
+                    var domain = zone.getName();
+                    domain = domain.substring(0, domain.length() - 1);
+                    this.zoneMap.put(domain, zone.getId());
                 }
             }
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
-        } finally {
-            this.closeHttpResponse(httpResponse);
         }
     }
 
     public HuaweiDataResult findDnsRecord(String domain, DNSRecordType recordType) {
+        final var httpRequestBase = this.getRequestBuilder()
+                .setUrl(this.getZoneUrl(domain))
+                .addQueryStringParam("type", recordType.name())
+                .setMethod(MethodType.GET)
+                .build();
+        byte[] contentBytes = this.executeRequest(httpRequestBase);
+        return this.mapperResult(contentBytes, HuaweiDataResult.class);
+    }
+
+    private byte[] executeRequest(HttpRequestBase httpRequestBase) {
         CloseableHttpResponse httpResponse = null;
         try {
-            final var httpRequestBase = this.getRequestBuilder()
-                                            .setUrl(api + "/" + this.zoneMap.get(domain) + "/recordsets")
-                                            .addQueryStringParam("type", recordType.name())
-                                            .setMethod(MethodType.GET)
-                                            .build();
             httpResponse = this.closeableHttpClient.execute(httpRequestBase);
-            final var contentBytes = this.getContentBytes(httpResponse);
-            return this.mapperResult(contentBytes, HuaweiDataResult.class);
-        } catch (Exception e) {
+            return this.getContentBytes(httpResponse);
+        } catch (IOException e) {
             log.error(e.getMessage(), e.getCause());
             return null;
         } finally {
@@ -93,12 +87,20 @@ public class HuaweiDnsAPI extends AbstractDnsAPI {
 
     private byte[] getContentBytes(CloseableHttpResponse httpResponse) throws IOException {
         if (httpResponse.getStatusLine()
-                        .getStatusCode() == 200) {
+                .getStatusCode() == 200) {
             return httpResponse.getEntity()
-                               .getContent()
-                               .readAllBytes();
+                    .getContent()
+                    .readAllBytes();
         }
         return new byte[0];
+    }
+
+    private String getZoneUrl(String domain) {
+        String zoneId = this.zoneMap.get(domain);
+        if (StringUtil.isEmpty(zoneId)) {
+            throw new RuntimeException("Resolved primary domain name:" + domain + "does not exist");
+        }
+        return this.api + "/" + zoneId + "/recordsets";
     }
 
     private void closeHttpResponse(CloseableHttpResponse response) {
